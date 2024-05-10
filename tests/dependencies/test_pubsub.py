71261a9555e798f71c765f0847e343d21e6e1da4
Juliano Fernandes <julianofernandes@gmail.com>
2024-05-10 15:40:25 -0300

    fix: pubsub dependency test

--- tests/dependencies/test_pubsub.py
+++ tests/dependencies/test_pubsub.py
@@ -1,5 +1,5 @@
 import json
-from typing import Any, Dict, Tuple
+from unittest.mock import MagicMock
 
 import pytest
 from fastapi.encoders import jsonable_encoder
@@ -8,30 +8,25 @@ from pydantic import BaseModel
 from fastapi_utils.dependencies import pubsub
 from fastapi_utils.protocols import Publisher
 
+MAGIC_NUMBER = "42"
+
 
 class MessageTest(BaseModel):
     content: str
 
 
-class FakeFuture:
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.args = args
-        self.kwargs = kwargs
+@pytest.fixture
+def pubsub_mock(monkeypatch: pytest.MonkeyPatch) -> MagicMock:
+    FakePublisherClient = MagicMock()
+    client = FakePublisherClient.return_value
+    client.publish.return_value.result.return_value = MAGIC_NUMBER
 
-    def result(self) -> Tuple[Tuple[Any], Dict[str, Any]]:
-        return self.args, self.kwargs
+    monkeypatch.setattr("google.cloud.pubsub_v1.PublisherClient", FakePublisherClient)
 
-
-def fake_publish(*args: Any, **kwargs: Any) -> Any:
-    return FakeFuture(*args[1:], **kwargs)
+    return client
 
 
-@pytest.fixture(autouse=True)
-def patch_pubsub(monkeypatch: pytest.MonkeyPatch) -> None:
-    monkeypatch.setattr(pubsub.pubsub_v1.PublisherClient, "publish", fake_publish)
-
-
-def test_publisher():
+def test_publisher(pubsub_mock: MagicMock):
     publisher = pubsub.Publisher()
 
     assert isinstance(publisher, Publisher)
@@ -39,7 +34,10 @@ def test_publisher():
     topic = "projects/test-project/topics/test-topic"
     message = MessageTest(content="test")
     result = publisher.publish(topic, message)
+
+    assert result == MAGIC_NUMBER
+
     args = (topic,)
     kwargs = {"data": json.dumps(jsonable_encoder(message)).encode()}
 
-    assert result == (args, kwargs)
+    assert pubsub_mock.publish.called_once_with(*args, **kwargs)
