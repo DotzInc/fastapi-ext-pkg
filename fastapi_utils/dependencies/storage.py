43d77e6854aad5897b8f00e26cee72416c52233a
Juliano Fernandes <julianofernandes@gmail.com>
2024-05-14 18:50:06 -0300

    feat: remove code who was moved to another package
    * Everything cloud related moved to the cloud-toolbox package: https://github.com/DotzInc/fastapi-utils

--- fastapi_utils/dependencies/storage.py
+++
@@ -1,7 +0,0 @@
-from typing import Type
-
-from fastapi_utils.services.protocols import Uploader
-
-
-def uploader(service: Type[Uploader]) -> Type[Uploader]:
-    return service
--- tests/dependencies/test_storage.py
+++
@@ -1,8 +0,0 @@
-from fastapi_utils.dependencies import storage
-from fastapi_utils.services import cloud_storage, protocols
-
-
-def test_uploader():
-    uploader = storage.uploader(cloud_storage.Uploader)
-
-    assert isinstance(uploader(), protocols.Uploader)
