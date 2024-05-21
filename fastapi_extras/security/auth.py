fbc1e48e9e1f4ffbf65739b73d65ea9efefde33b
Juliano Fernandes <julianofernandes@gmail.com>
2024-05-21 18:14:17 -0300

    refactor: use closure instead of class

--- fastapi_extras/security/auth.py
+++ fastapi_extras/security/auth.py
@@ -1,16 +1,52 @@
-from typing import Any, Mapping, Optional
+import logging
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+    Union,
+)
 
 import httpx
-from httpx import Response
+from fastapi import Depends, HTTPException, Request, status
+from fastapi.security import APIKeyCookie, APIKeyHeader, APIKeyQuery
+from typing_extensions import Annotated
+
+APIKeyScheme = Union[APIKeyCookie, APIKeyHeader, APIKeyQuery]
+Authorizer = Callable[[Request, str], Awaitable[Any]]
+
+DEFAULT_SCHEME = APIKeyHeader(name="Authorization")
+UNAUTHORIZED = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+
+logger = logging.getLogger(__name__)
 
 
-class RemoteAuthorization:
-    def __init__(self, url: str, **kwargs: Any) -> None:
-        self.url = url
-        self.kwargs = kwargs
+def remote_authorization(
+    url: str, scheme: APIKeyScheme = DEFAULT_SCHEME, **kwargs: Any
+) -> Authorizer:
+    assert isinstance(scheme, (APIKeyCookie, APIKeyHeader, APIKeyQuery)), "Invalid APIKeyScheme"
 
-    async def authorize(
-        self, data: Optional[Mapping[str, Any]] = None, json: Any = None
-    ) -> Response:
+    async def authorizer(request: Request, _token: Annotated[str, Depends(scheme)]) -> Any:
+        # TODO: Add support for caching
+
+        info = {
+            "method": request.method,
+            "path": request.url.path,
+            "query_params": dict(request.query_params),
+            "headers": dict(request.headers),
+            "cookies": request.cookies,
+        }
+
         async with httpx.AsyncClient() as client:
-            return await client.post(self.url, data=data, json=json, **self.kwargs)
+            try:
+                response = await client.post(url, json=info, **kwargs)
+                response.raise_for_status()
+
+                data = response.json()
+                request.scope["authorizer"] = data
+
+                return data
+            except httpx.HTTPError as http_error:
+                logger.error(http_error)
+                raise UNAUTHORIZED
+
+    return authorizer
--- tests/security/test_auth.py
+++ tests/security/test_auth.py
@@ -1,22 +1,80 @@
+from typing import Any, Dict
 from unittest.mock import AsyncMock
 
 import pytest
-from httpx import Request, Response, codes
+from fastapi import Request
+from httpx import Request as HTTPXRequest
+from httpx import Response as HTTPXResponse
+from httpx import codes
 
-from fastapi_extras.security.auth import RemoteAuthorization
+from fastapi_extras.security import auth
+
+AUTH_URL = "http://auth.test"
+AUTH_KEY = "supers3cr3t"
+USER_KEY = "cmFwYWR1cmEK"
+
+
+@pytest.fixture
+def scope() -> Dict[str, Any]:
+    return {
+        "type": "http",
+        "method": "GET",
+        "path": "/items/",
+        "query_string": "",
+        "headers": [(b"authorization", USER_KEY.encode())],
+    }
+
+
+@pytest.fixture
+def payload(scope) -> Dict[str, Any]:
+    return {
+        "method": scope["method"],
+        "path": scope["path"],
+        "query_params": {},
+        "headers": {k.decode(): v.decode() for k, v in scope["headers"]},
+        "cookies": {},
+    }
 
 
 @pytest.mark.anyio
-async def test_remote_authorization(httpx_asyncmock: AsyncMock):
-    url = "http://test"
-    headers = {"x-api-key": "supers3cr3t"}
-    payload = {"token": "cmFwYWR1cmEK"}
+async def test_remote_authorization(
+    scope: Dict[str, Any],
+    payload: Dict[str, Any],
+    httpx_asyncmock: AsyncMock,
+):
+    httpx_request = HTTPXRequest("POST", AUTH_URL, json=payload, headers={"x-api-key": AUTH_KEY})
+    httpx_response = HTTPXResponse(request=httpx_request, status_code=codes.OK, json={"foo": "bar"})
+    httpx_asyncmock.return_value = httpx_response
 
-    request = Request("POST", url, json=payload, headers=headers)
-    response = Response(request=request, status_code=codes.OK)
-    httpx_asyncmock.return_value = response
+    authorizer = auth.remote_authorization(url=AUTH_URL, headers={"x-api-key": AUTH_KEY})
+    request = Request(scope=scope)
+    response = await authorizer(request, USER_KEY)
 
-    authority = RemoteAuthorization(url, headers=headers)
-    result = await authority.authorize(json=payload)
+    assert response == {"foo": "bar"}
+    assert request.scope["authorizer"] == response
+    assert httpx_asyncmock.call_count == 1
+    assert httpx_asyncmock.call_args.args == ("POST", AUTH_URL)
+    assert httpx_asyncmock.call_args.kwargs["json"] == payload
+    assert httpx_asyncmock.call_args.kwargs["headers"] == {"x-api-key": AUTH_KEY}
 
-    assert result.status_code == codes.OK
+
+@pytest.mark.anyio
+async def test_remote_authorization_raises_401_unauthorized(
+    scope: Dict[str, Any],
+    payload: Dict[str, Any],
+    httpx_asyncmock: AsyncMock,
+):
+    httpx_request = HTTPXRequest("POST", AUTH_URL, json=payload, headers={"x-api-key": AUTH_KEY})
+    httpx_response = HTTPXResponse(request=httpx_request, status_code=codes.UNAUTHORIZED, json={})
+    httpx_asyncmock.return_value = httpx_response
+
+    authorizer = auth.remote_authorization(url=AUTH_URL, headers={"x-api-key": AUTH_KEY})
+    request = Request(scope=scope)
+
+    with pytest.raises(type(auth.UNAUTHORIZED), match=auth.UNAUTHORIZED.detail):
+        await authorizer(request, USER_KEY)
+
+    assert httpx_asyncmock.call_count == 1
+    assert httpx_asyncmock.call_args.args == ("POST", AUTH_URL)
+    assert httpx_asyncmock.call_args.kwargs["json"] == payload
+    assert httpx_asyncmock.call_args.kwargs["headers"] == {"x-api-key": AUTH_KEY}
